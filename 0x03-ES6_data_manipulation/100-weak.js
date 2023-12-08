445b2eb084f819a563fe1bf1614f37650041a637
KGiveso <mogoboyagiven@gmail.com>
2023-12-07 17:14:52 -0800

    no issues we share

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+/**
+ * A weak map of endpoints and the number of calls made.
+ */
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
+  }
+}
